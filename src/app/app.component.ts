cfbc3c131eb1c6ffa32df589b58c982343591404
Sebastian Gsellmann <sirvannos@hotmail.com>
2021-05-02 15:46:03 -0400

    fix: try to fix websockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { InfoMqttMessage } from "@mopopinball/engine/src/system/messages"
 import {ClientDevice} from '@mopopinball/engine/src/system/server/client-device';
 import {DipSwitchState} from '@mopopinball/engine/src/system/dip-switch-state';
@@ -9,6 +8,7 @@ import {AvailableUpdate} from '@mopopinball/engine/src/system/server/available-u
 import {UpdateDetails} from '@mopopinball/engine/src/system/server/update-details';
 import { HttpClient } from '@angular/common/http';
 import {version} from 'package.json';
+import {connect} from 'mqtt';
 
 @Component({
     selector: 'app-root',
@@ -28,47 +28,51 @@ export class AppComponent implements OnDestroy {
     rows = [0,1,2,3,4,5,6,7,8];
     cols = [1,2,3,4,5,6,7,8];
     systemUpdateInProgress = false;
+    private _mqttService = connect(`ws://${document.location.hostname}`);
 
-    constructor(private _mqttService: MqttService, private http: HttpClient) {
-        this._mqttService.observe('mopo/info/general').subscribe((message: IMqttMessage) => {
-            this.info = JSON.parse(message.payload.toString());
+    constructor(private http: HttpClient) {
+        this._mqttService.subscribe('mopo/info/general', (topic, message) => {
+            this.info = JSON.parse(message.toString());
         });
-        this._mqttService.observe('mopo/info/fps').subscribe((message: IMqttMessage) => {
-            this.fps = JSON.parse(message.payload.toString());
-        });
+        // this._mqttService.observe('mopo/info/general').subscribe((message: IMqttMessage) => {
+        //     this.info = JSON.parse(message.payload.toString());
+        // });
+        // this._mqttService.observe('mopo/info/fps').subscribe((message: IMqttMessage) => {
+        //     this.fps = JSON.parse(message.payload.toString());
+        // });
 
         // Subscribe only long enough to get all initial state, then unsubscribe.
-        this.subscription = this._mqttService.observe('mopo/devices/+/all/state').subscribe((message: IMqttMessage) => {
-            switch (message.topic) {
-                case 'mopo/devices/lamps/all/state':
-                    this.lamps = JSON.parse(message.payload.toString());
-                break;
-                case 'mopo/devices/coils/all/state':
-                    this.coils = JSON.parse(message.payload.toString());
-                break;
-                case 'mopo/devices/sounds/all/state':
-                    this.sounds = JSON.parse(message.payload.toString());
-                break;
-                case 'mopo/devices/switches/all/state':
-                    this.switches = JSON.parse(message.payload.toString());
-                break;
-            }
-            if (this.lamps.length > 0 && this.coils.length > 0 && this.sounds.length > 0 && this.switches.length > 0) {
-                this.subscription.unsubscribe();
-            }
-        });
+        // this.subscription = this._mqttService.observe('mopo/devices/+/all/state').subscribe((message: IMqttMessage) => {
+        //     switch (message.topic) {
+        //         case 'mopo/devices/lamps/all/state':
+        //             this.lamps = JSON.parse(message.payload.toString());
+        //         break;
+        //         case 'mopo/devices/coils/all/state':
+        //             this.coils = JSON.parse(message.payload.toString());
+        //         break;
+        //         case 'mopo/devices/sounds/all/state':
+        //             this.sounds = JSON.parse(message.payload.toString());
+        //         break;
+        //         case 'mopo/devices/switches/all/state':
+        //             this.switches = JSON.parse(message.payload.toString());
+        //         break;
+        //     }
+        //     if (this.lamps.length > 0 && this.coils.length > 0 && this.sounds.length > 0 && this.switches.length > 0) {
+        //         this.subscription.unsubscribe();
+        //     }
+        // });
 
-        this._mqttService.observe('mopo/devices/+/+/state/update').subscribe((message: IMqttMessage) => {
-            const payload = JSON.parse(message.payload.toString());
-            this.update(this.lamps, payload);
-            this.update(this.coils, payload);
-            this.update(this.sounds, payload);
-            // this.update()
-        });
+        // this._mqttService.observe('mopo/devices/+/+/state/update').subscribe((message: IMqttMessage) => {
+        //     const payload = JSON.parse(message.payload.toString());
+        //     this.update(this.lamps, payload);
+        //     this.update(this.coils, payload);
+        //     this.update(this.sounds, payload);
+        //     // this.update()
+        // });
 
-        this._mqttService.observe('mopo/devices/dips/all/state').subscribe((message: IMqttMessage) => {
-            this.dips = JSON.parse(message.payload.toString());
-        });
+        // this._mqttService.observe('mopo/devices/dips/all/state').subscribe((message: IMqttMessage) => {
+        //     this.dips = JSON.parse(message.payload.toString());
+        // });
     }
 
     update(collection: any[], updates: any[]) {
@@ -86,7 +90,7 @@ export class AppComponent implements OnDestroy {
     }
 
     toggleDevice(device: ClientDevice): void {
-        this._mqttService.publish('mopo/devices/anytype/anyid/state/update/client', JSON.stringify(device)).subscribe();
+        // this._mqttService.publish('mopo/devices/anytype/anyid/state/update/client', JSON.stringify(device)).subscribe();
     }
 
     getSwitch(row: number, col: number): any {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import {
-  MqttModule,
-  IMqttServiceOptions
-} from 'ngx-mqtt';
-
-export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
-  hostname: document.location.hostname,
-  port: 9001
-};
+// export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+//   hostname: document.location.hostname,
+//   port: 9001
+// };
 
 import { AppComponent } from './app.component';
 import { OutputDeviceComponent } from './output-device/output-device.component';
@@ -22,8 +17,7 @@ import { OutputDeviceComponent } from './output-device/output-device.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
